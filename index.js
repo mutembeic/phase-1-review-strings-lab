a7268877d192d921585d2280030c87ab1eadb082
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-06-13 13:10:14 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "charles";
+const welcomeMessage = 'Welcome to Flatbook, charles!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, CHARLES!';
+const shortGreeting = 'Welcome, c!'
+
+
